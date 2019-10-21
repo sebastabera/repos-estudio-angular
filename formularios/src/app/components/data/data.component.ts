9f083b555f9cde83daa01bbec6e3fc273dcbc4ea
Luis Sebastian Tabera Quintero <lstabera@personalsoft.com.co>
2019-10-21 16:07:06 -0500

    formularios

--- formularios/src/app/components/data/data.component.ts
+++ formularios/src/app/components/data/data.component.ts
@@ -11,7 +11,7 @@ export class DataComponent implements OnInit {
 
   forma:FormGroup;
 
-  usuario:Object = {
+  usuario:any = {
     nombrecompleto: {
       nombre: 'Sebastian',
       apellido: 'Tabera'
@@ -22,10 +22,12 @@ export class DataComponent implements OnInit {
   constructor() { 
     this.forma = new FormGroup({
       'nombrecompleto': new FormGroup({
-        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-        'apellido': new FormControl('', Validators.required)}),      
-      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+        'nombre': new FormControl(this.usuario.nombrecompleto.nombre, [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl(this.usuario.nombrecompleto.apellido, Validators.required)}),      
+      'correo': new FormControl(this.usuario.correo, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
+
+    this.forma.setValue(this.usuario);
   }
 
   ngOnInit() {
@@ -33,6 +35,15 @@ export class DataComponent implements OnInit {
 
   guardarCambios(){
     console.log(this.forma.value);
+    //this.forma.reset(this.usuario);
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: "",
+        apellido: ""
+      },
+      correo: ""
+    });
+
   }
 
 }
